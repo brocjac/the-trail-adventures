9fe94dcc45238d72aef020af0fb59b619747acbd
brocjac <63880557+brocjac@users.noreply.github.com>
2026-09-12 20:58:10 -0500

    Nothing writes into the record any more

--- Project/trails.cs
+++ Project/trails.cs
@@ -1,11 +1,37 @@
+using System.Runtime.CompilerServices;
+
 public class Trails
 {
-    public string Name;
-    public string Location;
-    public string Surface;
-    public decimal TotalLengthMiles;
-    public string Activity;
-    public string Notes;
+    public string Surface { get; set; }
+    public decimal TotalLengthMiles { get; set; }
+    public string Activity { get; set; }
+    public string Notes { get; set; }
+
+    private string _name = "(unnamed)";
+    public string Name
+    {
+        get {return _name;}
+        set
+        {
+           if (!string.IsNullOrWhiteSpace(value))
+            {
+                _name = value.Trim();
+            } 
+        }
+    }
+
+    private string _location = "";
+    public string Location
+    {
+        get {return _location;}
+        set
+        {
+           if (!string.IsNullOrWhiteSpace(value))
+            {
+                _location = value.Trim();
+            } 
+        }
+    }
 
     public Trails(string name)
     {
